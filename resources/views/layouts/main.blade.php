334afde02b68b4f5b1b5fc1d5c0cdb810f45d3cc
Matheus-Baptista27 <matheus.baptista07@hotmail.com>
2026-03-16 17:20:54 -0300

    atualizando idioma para dispositivos móveis

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head> <meta charset="utf-8">
+<html lang="pt-BR">
+<head>  <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="Content-Language" content="pt-BR">
+        <meta name="google" content="notranslate">
+
         <title>@yield('title')</title>
          <!-- fonte google--> <!--<link rel="preconnect" href="https://fonts.googleapis.com"> <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>-->
 
