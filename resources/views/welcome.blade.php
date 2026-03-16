5c6ac7cc05ac8dda6ccf42d757f7b0f451986024
Matheus-Baptista27 <matheus.baptista07@hotmail.com>
2026-03-16 16:35:56 -0300

    atualizando tela de consulta

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 
 <div id="search-container" class="col-md-12">
-    <h1>Consulte um Instrutor</h1>
+    <h1>Consultar</h1>
 
     <form action="/" method="GET">
 
@@ -16,7 +16,7 @@
                 id="search"
                 name="search"
                 class="form-control"
-                placeholder="Nome do Instrutor ..."
+                placeholder="Colaborador ..."
                 value="{{ $search }}"
             >
         </div>
