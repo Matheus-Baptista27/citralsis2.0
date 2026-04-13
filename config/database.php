f4c2fb0f60bb67500b0be93ab9c93a197f8412f2
Matheus-Baptista27 <matheus.baptista07@hotmail.com>
2026-04-13 09:23:08 -0300

    remove DB_URL do pgsql config

--- config/database.php
+++ config/database.php
@@ -86,12 +86,16 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DB_URL'),
+            'host' => env('DB_HOST'),
+            'port' => env('DB_PORT'),
+            'database' => env('DB_DATABASE'),
+            'username' => env('DB_USERNAME'),
+            'password' => env('DB_PASSWORD'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => 'require',
+            'sslmode' => env('DB_SSLMODE', 'require'),
         ],
 
         'sqlsrv' => [
