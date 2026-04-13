6022f8dc51c5027f24981d6c333e3e4471c18f91
Matheus-Baptista27 <matheus.baptista07@hotmail.com>
2026-04-13 09:27:51 -0300

    fallback database config

--- config/database.php
+++ config/database.php
@@ -86,11 +86,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST'),
-            'port' => env('DB_PORT'),
-            'database' => env('DB_DATABASE'),
-            'username' => env('DB_USERNAME'),
-            'password' => env('DB_PASSWORD'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'neondb'), // 👈 FORÇA DEFAULT
+            'username' => env('DB_USERNAME', 'neondb_owner'),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
